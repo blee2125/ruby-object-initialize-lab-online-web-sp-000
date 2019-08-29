c7f7c5d46eba68ee4e272bc8c7d3733b1c5cda19
favorable-device-5158 <blee2125@gmail.com>
2019-08-29 20:04:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed= "Mutt")
     @name= name
+    @breed= breed
   end
   def name=(name)
     @name = name
@@ -9,9 +10,6 @@ class Dog
     @name
   end
 
-  def initialize(breed)
-    @breed = breed
-  end
 
   def breed=(breed)
     @breed = breed
